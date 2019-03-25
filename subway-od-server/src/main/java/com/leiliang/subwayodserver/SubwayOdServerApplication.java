f87f452d0c07cc0a20088f9aadc406bcadfffb38
SuperLeilia <1406859565@qq.com>
2019-03-25 22:00:51 +0800

    ‘增加README.md说明文档’

---
+++ subway-od-server/src/main/java/com/leiliang/subwayodserver/SubwayOdServerApplication.java
@@ -0,0 +1,13 @@
+package com.leiliang.subwayodserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayOdServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayOdServerApplication.class, args);
+    }
+
+}
