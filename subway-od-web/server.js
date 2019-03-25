aca7cb974d16811babb2bc037535281c99a297ec
SuperLeilia <1406859565@qq.com>
2019-03-25 22:49:27 +0800

    feat: add server.js

---
+++ subway-od-web/server.js
@@ -0,0 +1,16 @@
+var Koa = require('koa');
+var path = require('path');
+var server = require('koa-static');
+
+var app = new Koa();
+
+// 静态资源目录对于相对入口文件index.js的路径
+var staticPath = './dist/subway-od-web';
+
+app.use(server(
+  path.join( __dirname,  staticPath)
+));
+
+app.listen(3000, function () {
+  console.log('[demo] static-use-middleware is starting at port 3000')
+});
